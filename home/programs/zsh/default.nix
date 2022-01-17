0e21f12402c0152ff9461c6e6c1197b6cb438c75
Guangqing Chen <hi@goushi.me>
2022-01-18 00:47:25 +0800

    [zsh] change theme from agnoster to powerlevel10k

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -12,7 +12,26 @@ in {
       plugins = [
         "git" "z" "extract" "sudo" "colored-man-pages" "history-substring-search"
       ];
-      theme = "agnoster";
     };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
   };
 }

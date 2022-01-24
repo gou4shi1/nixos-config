1e000832856d5eaf92378ae9955910a2b5d7b534
Guangqing Chen <hi@goushi.me>
2022-01-25 00:19:25 +0800

    [shell] add bat

--- home/programs/fzf/default.nix
+++ home/programs/fzf/default.nix
@@ -7,11 +7,19 @@ in {
     ripgrep highlight bfs
   ];
 
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "Nord";
+    };
+  };
+
   programs.fzf = {
     enable = true;
     defaultCommand = "rg --files --hidden";
     defaultOptions = [ "--bind ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" ];
     fileWidgetCommand = "rg --files --hidden";
+    # TODO: change highlight to bat as https://github.com/sharkdp/bat/issues/357#issuecomment-555971886
     fileWidgetOptions = [ "--height 40%" "--layout=reverse" "--preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'" ];
     changeDirWidgetCommand = "bfs -type d -nohidden | sed s#^\\./##";
   };

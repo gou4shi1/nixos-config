2e0432915951d37c16d3dc380487f73b8ec8f53b
Guangqing Chen <hi@goushi.me>
2022-02-11 22:56:49 +0800

    [hm] add ctags and gtags

--- home/programs/vim/default.nix
+++ home/programs/vim/default.nix
@@ -4,6 +4,16 @@ let
 
 in {
   home.packages = with pkgs; [
-    pack nodejs shellcheck rnix-lsp
+    vimHugeX # Huge version with GTK3 GUI.
+    pack # A plugins manager.
+    nodejs # For CoC.
+    universal-ctags # A maintained ctags implementation.
+    global # Gtags, a global source code tagging system.
+    shellcheck # A shell script analysis tool.
+    rnix-lsp # A language server for Nix.
   ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
 }

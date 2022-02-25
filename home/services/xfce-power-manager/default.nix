838493348c037efcf7ab9c5a76e7fddae0ff91f0
Guangqing Chen <hi@goushi.me>
2022-02-25 23:14:02 +0800

    [hm] only enable late-locking on laptop

--- home/services/xfce-power-manager/default.nix
+++ home/services/xfce-power-manager/default.nix
@@ -6,5 +6,14 @@
       workstation = ./workstation-conf.xml;
       laptop = ./laptop-conf.xml;
     });
+
+    dconf.settings = {
+      "apps/light-locker" = {
+        # If late-locking is true, light-locker will lock the session when the screensaver is deactivated.
+        # If it's false, light-locker will lock the session when the screensaver is activated,
+        # then xfce4-power-manager will have no authorization to suspend the system after timeout.
+        late-locking = (machineType == "laptop");
+      };
+    };
   };
 }

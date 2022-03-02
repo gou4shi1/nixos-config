f41c3bad453ec9003eda8dc32271f702e4c79090
Guangqing Chen <hi@goushi.me>
2022-03-02 18:42:43 +0800

    [wr] add zoom

--- machines/wr-laptop/home.nix
+++ machines/wr-laptop/home.nix
@@ -5,6 +5,10 @@ let
 
 in {
   home-manager.users."${cfg.mainUser}" = {
+    home.packages = with pkgs; [
+      zoom-us
+    ];
+
     programs.git = {
       userName = "Guangqing Chen";
       userEmail = "hi@goushi.me";

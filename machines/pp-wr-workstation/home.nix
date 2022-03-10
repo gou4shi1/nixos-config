c4208e28c9342c0102c6844ecc1c635fd96683f8
Chen Jingpiao <chenjingpiao@gmail.com>
2022-03-10 21:07:04 +0800

    [machines] add pp-wr-workstation

---
+++ machines/pp-wr-workstation/home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+let
+  cfg = config.mynix;
+
+in {
+  home-manager.users."${cfg.mainUser}" = {
+    imports = [
+      ../../home/programs/dev/bazel.nix
+      ../../home/programs/dev/cpp.nix
+    ];
+
+    home.packages = with pkgs; [
+      debian-hostname zoom-us
+    ];
+
+    home.sessionVariables = {
+      JC_CAR_ID = "GZU_SPY_10036";
+    };
+
+    programs.zsh.initExtra = ''
+      source ~/temp/gde.sh no_extra_opt_jc /run/user/1000 /etc/fonts
+    '';
+
+    programs.git = {
+      userName = "Jingpiao Chen";
+      userEmail = "jingpiao.chen@weride.ai";
+    };
+  };
+}

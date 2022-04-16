9297e0add54f8494b5dd632f7d6d1da84f43d995
Aria <461863631@qq.com>
2022-04-16 13:34:27 +0800

    [xk-mi-nb] enlarge the font size of tilix

--- machines/xk-mi-notebook/home.nix
+++ machines/xk-mi-notebook/home.nix
@@ -21,5 +21,11 @@ in {
     # Add japanese input method.
     i18n.inputMethod.fcitx5.addons = with pkgs; [ fcitx5-mozc ];
     xdg.configFile."fcitx5/profile".source = ./fcitx-profile;
+
+    dconf.settings = {
+      "com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d" = {
+        font = "Monospace 17";
+      };
+    };
   };
 }

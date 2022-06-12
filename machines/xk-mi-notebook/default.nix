e9a8a285c3b75db74bd9ee9574de60a69ecbe3f6
Guangqing Chen <hi@goushi.me>
2022-06-12 17:12:40 +0800

    [machines] move the dhcp configuration to hardware-configuration.nix

--- machines/xk-mi-notebook/default.nix
+++ machines/xk-mi-notebook/default.nix
@@ -34,12 +34,6 @@ in {
   networking.hostName = "xk-mi-notebook";
   networking.networkmanager.enable = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp2s0.useDHCP = true;
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";

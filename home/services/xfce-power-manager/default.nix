5c21ec6b771b67433490721521bc2a89530dafd4
Guangqing Chen <hi@goushi.me>
2022-06-25 17:13:26 +0800

    [desktop] fix power-manager failed to suspend after lock

--- home/services/xfce-power-manager/default.nix
+++ home/services/xfce-power-manager/default.nix
@@ -10,9 +10,8 @@
     dconf.settings = {
       "apps/light-locker" = {
         # If late-locking is true, light-locker will lock the session when the screensaver is deactivated.
-        # If it's false, light-locker will lock the session when the screensaver is activated,
-        # then xfce4-power-manager will have no authorization to suspend the system after timeout.
-        late-locking = (machineType == "laptop");
+        # If it's false, light-locker will lock the session when the screensaver is activated.
+        late-locking = false;
       };
     };
   };

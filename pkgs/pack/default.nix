68078b85f097774a5541b816ad232c0590a6c172
Guangqing Chen <hi@goushi.me>
2022-10-16 22:58:47 +0800

    [pack] change to my fork

--- pkgs/pack/default.nix
+++ pkgs/pack/default.nix
@@ -7,13 +7,13 @@ rustPlatform.buildRustPackage rec {
   version = "0.2.5";
 
   src = fetchFromGitHub {
-    owner = "maralla";
+    owner = "gou4shi1";
     repo = "pack";
-    rev = "84f07738c0c6f74c1608235a98388cbcc5e9efea";
-    sha256 = "0zdzy2blqvbdbxcvd4q70b5ldbn13nr7597p2fb0lvf3bb83qvwv";
+    rev = "28c48a73ecfb7e5b80e32da52de21f4d767669bb";
+    hash = "sha256-A8grXjYDbSyZjt8hGHBpndLg1bp4WG1SF1S4BQyP+kU=";
   };
 
-  cargoSha256 = "0awnfc0dlic76ysqnsnm0gpg582nhb80y82dqvj21sappc9xpn2w";
+  cargoHash = "sha256-01tvCAVNZEZgoj3J0GzkcDUvRwiEOEaZIIW/ZEVuiB8=";
 
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 

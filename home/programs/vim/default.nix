0379604d7c912d497372e0a599b3e8237e20a775
Guangqing Chen <hi@goushi.me>
2022-10-24 11:51:30 +0800

    [hm][vim] add typos

--- home/programs/vim/default.nix
+++ home/programs/vim/default.nix
@@ -9,6 +9,7 @@ in {
     universal-ctags # A maintained ctags implementation.
     global # Gtags, a global source code tagging system.
     rnix-lsp # A language server for Nix.
+    typos # A source code spell checker.
   ] ++ (if stdenv.isDarwin then [ vim-darwin ] else [ vimHugeX ]);
 
   home.sessionVariables = {

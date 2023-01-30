a1f8b9242b1d932298d45f51301d5d358387467b
Guangqing Chen <hi@goushi.me>
2023-01-30 15:43:38 +0800

    [laptop] upstream globalprotect-openconnect

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,6 +6,7 @@ let
 in {
   vaapiIntel = prev.vaapiIntel.override { enableHybridCodec = true; };
   feishu = unstable.feishu.override { commandLineArgs = "--use-gl=desktop --disable-features=AudioServiceSandbox"; nss = prev.nss_latest; };
+  globalprotect-openconnect = unstable.globalprotect-openconnect;
   warpd = unstable.warpd;
   rofi-power = final.callPackage ../pkgs/rofi-power {};
   i3-get-window-criteria = final.callPackage ../pkgs/i3-get-window-criteria {};
@@ -14,7 +15,6 @@ in {
   debian-hostname = final.callPackage ../pkgs/debian-hostname {};
   typora = final.callPackage ../pkgs/typora {};
   weggli = final.callPackage ../pkgs/weggli {};
-  globalprotect-openconnect = final.libsForQt5.callPackage ../pkgs/globalprotect-openconnect {};
   nomachine = final.callPackage ../pkgs/nomachine {};
   coscli = final.callPackage ../pkgs/coscli {};
   clangd = final.callPackage ../pkgs/clangd {};

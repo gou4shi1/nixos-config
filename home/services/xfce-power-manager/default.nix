db7145b8a1c3225dab6ee3fa2b65df4e36c5d589
Guangqing Chen <hi@goushi.me>
2023-05-04 16:02:55 +0800

    [hm] fix xfconf uint

--- home/services/xfce-power-manager/default.nix
+++ home/services/xfce-power-manager/default.nix
@@ -2,35 +2,35 @@
 
 let
   workstationConf = {
-    "xfce4-power-manager/battery-button-action" = 3;
+    "xfce4-power-manager/battery-button-action" = { type = "uint"; value = 3; };
     "xfce4-power-manager/blank-on-ac" = 10;
-    "xfce4-power-manager/dpms-on-ac-off" = 20;
-    "xfce4-power-manager/dpms-on-ac-sleep" = 15;
-    "xfce4-power-manager/hibernate-button-action" = 3;
-    "xfce4-power-manager/inactivity-on-ac" = 14;
-    "xfce4-power-manager/power-button-action" = 3;
-    "xfce4-power-manager/sleep-button-action" = 3;
+    "xfce4-power-manager/dpms-on-ac-off" = { type = "uint"; value = 20; };
+    "xfce4-power-manager/dpms-on-ac-sleep" = { type = "uint"; value = 15; };
+    "xfce4-power-manager/hibernate-button-action" = { type = "uint"; value = 3; };
+    "xfce4-power-manager/inactivity-on-ac" = { type = "uint"; value = 14; };
+    "xfce4-power-manager/power-button-action" = { type = "uint"; value = 3; };
+    "xfce4-power-manager/sleep-button-action" = { type = "uint"; value = 3; };
   };
   laptopConf = {
-    "xfce4-power-manager/battery-button-action" = 3;
+    "xfce4-power-manager/battery-button-action" = { type = "uint"; value = 3; };
     "xfce4-power-manager/blank-on-battery" = 5;
-    "xfce4-power-manager/brightness-on-ac" = 120;
-    "xfce4-power-manager/brightness-on-battery" = 120;
-    "xfce4-power-manager/critical-power-action" = 3;
-    "xfce4-power-manager/critical-power-level" = 5;
-    "xfce4-power-manager/dpms-on-ac-off" = 20;
-    "xfce4-power-manager/dpms-on-ac-sleep" = 15;
-    "xfce4-power-manager/dpms-on-battery-off" = 15;
-    "xfce4-power-manager/dpms-on-battery-sleep" = 10;
+    "xfce4-power-manager/brightness-on-ac" = { type = "uint"; value = 120; };
+    "xfce4-power-manager/brightness-on-battery" = { type = "uint"; value = 120; };
+    "xfce4-power-manager/critical-power-action" = { type = "uint"; value = 3; };
+    "xfce4-power-manager/critical-power-level" = { type = "uint"; value = 5; };
+    "xfce4-power-manager/dpms-on-ac-off" = { type = "uint"; value = 20; };
+    "xfce4-power-manager/dpms-on-ac-sleep" = { type = "uint"; value = 15; };
+    "xfce4-power-manager/dpms-on-battery-off" = { type = "uint"; value = 15; };
+    "xfce4-power-manager/dpms-on-battery-sleep" = { type = "uint"; value = 10; };
     "xfce4-power-manager/general-notification" = true;
     "xfce4-power-manager/handle-brightness-keys" = true;
-    "xfce4-power-manager/hibernate-button-action" = 3;
-    "xfce4-power-manager/inactivity-on-ac" = 60;
-    "xfce4-power-manager/inactivity-on-battery" = 30;
-    "xfce4-power-manager/inactivity-sleep-mode-on-battery" = 1;
-    "xfce4-power-manager/power-button-action" = 3;
+    "xfce4-power-manager/hibernate-button-action" = { type = "uint"; value = 3; };
+    "xfce4-power-manager/inactivity-on-ac" = { type = "uint"; value = 60; };
+    "xfce4-power-manager/inactivity-on-battery" = { type = "uint"; value = 30; };
+    "xfce4-power-manager/inactivity-sleep-mode-on-battery" = { type = "uint"; value = 1; };
+    "xfce4-power-manager/power-button-action" = { type = "uint"; value = 3; };
     "xfce4-power-manager/show-tray-icon" = true;
-    "xfce4-power-manager/sleep-button-action" = 3;
+    "xfce4-power-manager/sleep-button-action" = { type = "uint"; value = 3; };
   };
 
 in

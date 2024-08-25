cd2ec8a3f9081a7b4a1542ae60d0faf4bb6500d9
Guangqing Chen <hi@goushi.me>
2024-08-25 12:28:00 +0800

    [wr-laptop] fix kernel panic caused by nvidia driver

--- machines/wr-laptop/hardware-configuration.nix
+++ machines/wr-laptop/hardware-configuration.nix
@@ -32,6 +32,14 @@ in {
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "550.107.02";
+      sha256_64bit = "sha256-+XwcpN8wYCjYjHrtYx+oBhtVxXxMI02FO1ddjM5sAWg=";
+      sha256_aarch64 = "sha256-mVEeFWHOFyhl3TGx1xy5EhnIS/nRMooQ3+LdyGe69TQ=";
+      openSha256 = "sha256-Po+pASZdBaNDeu5h8sgYgP9YyFAm9ywf/8iyyAaLm+w=";
+      settingsSha256 = "sha256-WFZhQZB6zL9d5MUChl2kCKQ1q9SgD0JlP4CMXEwp2jE=";
+      persistencedSha256 = "sha256-Vz33gNYapQ4++hMqH3zBB4MyjxLxwasvLzUJsCcyY4k=";
+    };
   };
 
   services.xserver.dpi = 120;

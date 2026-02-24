01e586807f80cad96febc217384f90a3d6b5bb7b
Guangqing Chen <hi@goushi.me>
2026-02-24 23:14:20 +0800

    [hidpi] temp fix chrome scale factor

--- overlays/hidpi.nix
+++ overlays/hidpi.nix
@@ -1,11 +1,21 @@
 dpi: final: prev:
 
+let
+  scaleArg = "--force-device-scale-factor=${toString (dpi / 96)}";
+in
 {
   xfce = prev.xfce // {
     xfce4-settings = prev.xfce.xfce4-settings.overrideAttrs (old: {
-      patches = old.patches or [ ] ++ [(prev.replaceVars ../pkgs/xfce4-settings/hidpi.diff {
-        inherit dpi;
-      })];
+      patches = old.patches or [ ] ++ [
+        (prev.replaceVars ../pkgs/xfce4-settings/hidpi.diff {
+          inherit dpi;
+        })
+      ];
     });
   };
+} // prev.lib.optionalAttrs (dpi >= 192) {
+  google-chrome = prev.google-chrome.override { commandLineArgs = scaleArg; };
+  brave = prev.brave.override { commandLineArgs = scaleArg; };
+  feishu = prev.feishu.override { commandLineArgs = scaleArg; };
+  qq = prev.qq.override { commandLineArgs = scaleArg; };
 }
